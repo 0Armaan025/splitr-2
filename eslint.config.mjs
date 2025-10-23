c1f17d18a26ee552d6676a878e8f53df31b0cc56
0Armaan025 <armaan33000@gmail.com>
2025-10-23 20:28:45 +0530

    || update -> removed eslint rules ||

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -16,6 +16,8 @@ const eslintConfig = [
     rules: {
       'react/no-unescaped-entities': 'off',
       '@next/next/no-page-custom-font': 'off',
+      '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+      '@typescripot-reslint/no-explicit-any': 'off',
     },
     ignores: [
       "node_modules/**",
